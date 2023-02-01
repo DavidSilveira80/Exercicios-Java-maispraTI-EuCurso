32bc6d22b93194714d037d0c04b75788f9fe4760
DavidSilveira80 <davidpscontato@gmail.com>
2023-02-01 18:50:10 -0300

    Removendo getters e setters desnecessários na classe Aluno.

--- src/models/Aluno.java
+++ src/models/Aluno.java
@@ -1,8 +1,8 @@
 package models;
 
 public class Aluno {
-    String nomeAluno;
-    double nota1, nota2, nota3, media, maiorNota, menorNota, notasSomadas;
+    private String nomeAluno;
+    private double nota1, nota2, nota3, media, maiorNota, menorNota, notasSomadas;
 
     public Aluno(String nomeAluno, double nota1, double nota2, double nota3){
         this.nomeAluno = nomeAluno;
@@ -11,68 +11,8 @@ public class Aluno {
         this.nota3 = nota3;
     }
 
-    public String getNomeAluno() {
-        return nomeAluno;
-    }
-
-    public void setNomeAluno(String nomeAluno) {
-        this.nomeAluno = nomeAluno;
-    }
-
-    public double getNota1() {
-        return nota1;
-    }
-
-    public void setNota1(double nota1) {
-        this.nota1 = nota1;
-    }
-
-    public double getNota2() {
-        return nota2;
-    }
-
-    public void setNota2(double nota2) {
-        this.nota2 = nota2;
-    }
-
-    public double getNota3() {
-        return nota3;
-    }
-
-    public void setNota3(double nota3) {
-        this.nota3 = nota3;
-    }
-
-    public double getMedia() {
-        return media;
-    }
-
-    public void setMedia(double media) {
-        this.media = media;
-    }
-
-    public double getMaiorNota() {
-        return maiorNota;
-    }
-
-    public void setMaiorNota(double maiorNota) {
-        this.maiorNota = maiorNota;
-    }
-
-    public double getMenorNota() {
-        return menorNota;
-    }
-
-    public void setMenorNota(double menorNota) {
-        this.menorNota = menorNota;
-    }
-
     public double getNotasSomadas() {
         return notasSomadas;
-    }
-
-    public void setNotasSomadas(double notasSomadas) {
-        this.notasSomadas = notasSomadas;
     }
 
     public double somarNotas(){
