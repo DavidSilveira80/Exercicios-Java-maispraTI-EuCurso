030cd5e34c92e0d0b3e47de82aed58d02c6b7e8a
DavidSilveira80 <davidpscontato@gmail.com>
2023-02-21 17:21:31 -0300

    Ajustes nos métodos do exercicio7.

--- src/main/java/exercicios/Exercicio7.java
+++ src/main/java/exercicios/Exercicio7.java
@@ -2,9 +2,7 @@ package exercicios;
 
 import models.Pedidos;
 import utils.Status;
-
 import static java.lang.System.out;
-import static utils.Entradas.*;
 import static utils.Entradas.entradaInteira;
 import static utils.Menus.mostrarMenuLanchesValor;
 import static utils.Menus.mostrarMenuPrincipalLancheria;
@@ -94,5 +92,81 @@ public class Exercicio7 {
     public static void chamadaExercicio7(){
         out.println("\nEXERCÍCIO POO 4\n");
         executarExercicio7Poo4Pedido();
+    }
+
+    public static String selecionarPedidoItem(int codigo, Pedidos pedido){
+        String item = "";
+        switch(codigo){
+            case 1:
+                item = "X SALADA";
+                break;
+            case 2:
+                item = "X FRANGO";
+                break;
+            case 3:
+                item = "X CALABRESA";
+                break;
+            case 4:
+                item = "X CORAÇÃO";
+                break;
+            case 5:
+                item = "X BACON";
+                break;
+            case 6:
+                item = "CACHORRO-QUENTE SIMPLES";
+                break;
+            case 7:
+                item = "CACHORRO-QUENTE DUPLO";
+                break;
+            case 8:
+                item = "PIZZAS TODOS OS SABORES";
+                break;
+            case 9:
+                item = "REFRIGERANTE";
+                break;
+            case 10:
+                item = "CERVEJA";
+                break;
+        }
+        pedido.setIten(item);
+        return item;
+    }
+
+    public static double selecionarValorItemPedido(String item, Pedidos pedido){
+        double valorItem = 0.0;
+        switch(item){
+            case "X SALADA":
+                valorItem = 18.00;
+                break;
+            case "X FRANGO":
+                valorItem = 19.00 ;
+                break;
+            case "X CALABRESA":
+                valorItem = 20.00;
+                break;
+            case  "X CORAÇÃO":
+                valorItem = 20.00;
+                break;
+            case "X BACON":
+                valorItem = 22.00;
+                break;
+            case "CACHORRO-QUENTE SIMPLES":
+                valorItem = 15.00;
+                break;
+            case "CACHORRO-QUENTE DUPLO":
+                valorItem = 17.00;
+                break;
+            case "PIZZAS TODOS OS SABORES":
+                valorItem = 30.00;
+                break;
+            case "REFRIGERANTE":
+                valorItem = 12.00;
+                break;
+            case "CERVEJA":
+                valorItem = 18.00;
+                break;
+        }
+        pedido.setValorDoIten(valorItem);
+        return valorItem;
     }
 }
--- src/main/java/utils/Entradas.java
+++ src/main/java/utils/Entradas.java
@@ -1,10 +1,6 @@
 package utils;
 
-import models.Pedidos;
 import static java.lang.System.out;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Entradas {
@@ -42,83 +38,6 @@ public class Entradas {
     public static String entradaString(){
         Scanner entrada =  new Scanner(System.in);
         return entrada.nextLine();
-    }
-
-
-    public static String selecionarPedidoItem(int codigo, Pedidos pedido){
-        String item = "";
-        switch(codigo){
-            case 1:
-                item = "X SALADA";
-                break;
-            case 2:
-                item = "X FRANGO";
-                break;
-            case 3:
-                item = "X CALABRESA";
-                break;
-            case 4:
-                item = "X CORAÇÃO";
-                break;
-            case 5:
-                item = "X BACON";
-                break;
-            case 6:
-                item = "CACHORRO-QUENTE SIMPLES";
-                break;
-            case 7:
-                item = "CACHORRO-QUENTE DUPLO";
-                break;
-            case 8:
-                item = "PIZZAS TODOS OS SABORES";
-                break;
-            case 9:
-                item = "REFRIGERANTE";
-                break;
-            case 10:
-                item = "CERVEJA";
-                break;
-        }
-        pedido.setIten(item);
-        return item;
-    }
-
-    public static double selecionarValorItemPedido(String item, Pedidos pedido){
-        double valorItem = 0.0;
-        switch(item){
-            case "X SALADA":
-                valorItem = 18.00;
-                break;
-            case "X FRANGO":
-                valorItem = 19.00 ;
-                break;
-            case "X CALABRESA":
-                valorItem = 20.00;
-                break;
-            case  "X CORAÇÃO":
-                valorItem = 20.00;
-                break;
-            case "X BACON":
-                valorItem = 22.00;
-                break;
-            case "CACHORRO-QUENTE SIMPLES":
-                valorItem = 15.00;
-                break;
-            case "CACHORRO-QUENTE DUPLO":
-                valorItem = 17.00;
-                break;
-            case "PIZZAS TODOS OS SABORES":
-                valorItem = 30.00;
-                break;
-            case "REFRIGERANTE":
-                valorItem = 12.00;
-                break;
-            case "CERVEJA":
-                valorItem = 18.00;
-                break;
-        }
-        pedido.setValorDoIten(valorItem);
-        return valorItem;
     }
 
     public static int informarFluxo1E2(){
