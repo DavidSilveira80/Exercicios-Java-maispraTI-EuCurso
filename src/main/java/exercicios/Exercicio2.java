e965646b141a53e0b06ab5e12c9ee50cc8de5495
DavidSilveira80 <davidpscontato@gmail.com>
2023-02-23 02:45:21 -0300

    Refatorado método do Exercicio2.

--- src/main/java/exercicios/Exercicio2.java
+++ src/main/java/exercicios/Exercicio2.java
@@ -14,14 +14,8 @@ public class Exercicio2 {
             out.print("INFORME A QUANTIDADE DE MAÇÃS QUE VOCÊ QUER COMPRAR: ");
             int quantidadeMacas = entradaInteira();
 
-            double valorApartirDeUmaDuzia = 0.25; double valorAbaixoDeUmaDuzia = 0.30;
-
-            if (quantidadeMacas >= 12) {
-                out.println(rValorDasMacas(quantidadeMacas, valorApartirDeUmaDuzia));
-
-            } else {
-                out.println(rValorDasMacas(quantidadeMacas, valorAbaixoDeUmaDuzia));
-            }
+            out.println(rValorDasMacas(quantidadeMacas,
+                    calcularValorTotalMacas(quantidadeMacas)));
 
             option = informarFluxo1E2();
         }while(option != 2);
@@ -29,18 +23,18 @@ public class Exercicio2 {
 
     //------------------------------------MÉTODOS AUXILIARES---------------------------------------------------
 
-    public static String rValorDasMacas(int macas, double valor) {
+    public static String rValorDasMacas(int macas, double valorTotal) {
 
-        return String.format("O VALOR TOTAL DAS %d MAÇÃS É DE R$ %.2f\n", macas, valor * macas);
+        return String.format("O VALOR TOTAL DAS %d MAÇÃS É DE R$ %.2f\n", macas, valorTotal);
    }
 
    public static double calcularValorTotalMacas(int quantidadeDeMacas){
-        double valorApartirDeUmaDuzia = 0.25;
-        double valorAbaixoDeUmaDuzia = 0.30;
+        double valorApartirDeUmaDuzia = 0.25; double valorAbaixoDeUmaDuzia = 0.30;
 
         double total = 0.0;
         if (quantidadeDeMacas >= 12){
             total = quantidadeDeMacas * valorApartirDeUmaDuzia;
+
         }else if(quantidadeDeMacas < 12){
             total = quantidadeDeMacas * valorAbaixoDeUmaDuzia;
         }
